8103af7ef769c3e3eaeb9f99f1d11e1e45222202
Jared Beagley <beagley.jared@gmail.com>
2020-10-02 17:04:43 -0600

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'benefits-estimator-ui';
+  employeeName: string;
+  numDependents: number;
+  income = 2000;
+  percentEmployeeBenefitsPaidByEmployer = 100;
+  percentDependantBenefitsPaidByEmployer = 50;
+  costOfEmployeeToBusiness: string;
+  employeePaycheckAmount: string;
+
+  calculateCost(): void {
+    this.costOfEmployeeToBusiness = (this.income
+      + (1000 * this.percentEmployeeBenefitsPaidByEmployer / 1200)
+      + (500 * this.percentDependantBenefitsPaidByEmployer / 1200)).toFixed(2);
+    this.employeePaycheckAmount = (this.income
+      - (1000 * (100 - this.percentEmployeeBenefitsPaidByEmployer) / 1200)
+      - (500 * (100 - this.percentDependantBenefitsPaidByEmployer) / 1200)).toFixed(2);
+  }
+
 }
