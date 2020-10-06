113e412cd695c3477506de8b9bcba5d59f40bc94
Jared Beagley <beagley.jared@gmail.com>
2020-10-06 07:37:08 -0600

    Started adding calculation service tests

--- src/app/services/calculation.service.spec.ts
+++ src/app/services/calculation.service.spec.ts
@@ -13,4 +13,41 @@ describe('CalculationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getAnnualCompanyCost should be the sum of all getAnnualAmountPaidByCompany', () => {
+    const MockGridData = [
+      {
+        name: 'Mr. Tester',
+        numDependants: 1,
+        annualSalary: 100000,
+        annualEmployeeBenefitsCost: 2000,
+        annualDependantBenefitsCost: 1000,
+        percentEmployeeBenefitsPaidByEmployer: 10,
+        percentDependantBenefitsPaidByEmployer: 5
+      },
+      {
+        name: 'Ms. Testing',
+        numDependants: 2,
+        annualSalary: 150000,
+        annualEmployeeBenefitsCost: 2000,
+        annualDependantBenefitsCost: 1000,
+        percentEmployeeBenefitsPaidByEmployer: 10,
+        percentDependantBenefitsPaidByEmployer: 5
+      }
+    ];
+    let accCost = 0;
+    MockGridData.forEach((rowData) => {
+      accCost += service.getAnnualAmountPaidByCompany(
+        rowData.annualSalary,
+        rowData.annualEmployeeBenefitsCost,
+        rowData.annualDependantBenefitsCost,
+        rowData.percentEmployeeBenefitsPaidByEmployer,
+        rowData.percentDependantBenefitsPaidByEmployer,
+        rowData.numDependants,
+        rowData.name
+      );
+    });
+    const annualCost = service.getAnnualCompanyCost(MockGridData);
+    expect(accCost).toEqual(annualCost);
+  });
 });
